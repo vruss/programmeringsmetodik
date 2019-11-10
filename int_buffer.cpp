0fc92f5a72ba2e2c26d769a15e13bd4ecf44bf23
Viktor Rosvall <virosen@yahoo.se>
2019-11-10 13:20:59 +0100

    first commit

---
+++ int_buffer.cpp
@@ -0,0 +1,8 @@
+//
+// Laboration: Dynamisk minneshantering, RAII och merge
+// Author: Viktor Rosvall
+// File name: int_buffer.cpp - created 2019-11-10 - modified 2019-11-10
+// Deklatation av klass int_buffer som sköter en minnesresurs av typen int.
+//
+
+#include "int_buffer.h"
