f5c44e9546d5e1ca5b9184b4390be5784a1d3797
Viktor Rosvall <virosen@yahoo.se>
2019-11-11 11:08:27 +0100

    copy constructor and first part done

--- int_buffer.h
+++ int_buffer.h
@@ -2,7 +2,7 @@
 // Laboration: Dynamisk minneshantering, RAII och merge
 // Author: Viktor Rosvall
 // File name: int_buffer.h - created 2019-11-10 - modified 2019-11-10
-// Definition av klass int_buffer som sköter en minnesresurs av typen int.
+// Prototyp av klass int_buffer som sköter en minnesresurs av typen int.
 //
 
 #ifndef LAB1_INT_BUFFER_H
@@ -12,18 +12,22 @@
 #include <cstdlib>
 
 class int_buffer {
+private:
+    int* int_p;
+    size_t length;
+
 public :
 
     // Constructors
-    explicit int_buffer(size_t size);
-    int_buffer(const int *source, size_t size);
+    explicit int_buffer(size_t size); // default constructor
+    int_buffer(const int *source, size_t size); // resize constructor
 
     int_buffer(const int_buffer &rhs); // copy constructor
     int_buffer(int_buffer &&rhs); // move constructor
 
     // Operator overloads
-    int_buffer &operator=(const int_buffer &rhs); // copy-assignment constructor
-    int_buffer &operator=(int_buffer &&rhs); // move-assign constructor
+    int_buffer &operator=(const int_buffer &rhs); // copy-assignment operator
+    int_buffer &operator=(int_buffer &&rhs); // move-assign operator
 
     // Methods
     size_t size() const;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "int_buffer.h"
+
+void testFunction(int_buffer buf);
 
 int main() {
 
+    int_buffer buff = int_buffer(10);
+    testFunction(buff);
+
     return 0;
+}
+
+void testFunction(int_buffer buf) {
+    // Set pointer values
+    int counter = 0;
+    for (int *i = buf.begin(); i != buf.end(); i++) {
+        *i = counter;
+        counter++;
+    }
+
+    // Print address and value
+    for (const int *i = buf.begin(); i != buf.end(); i++) {
+        std::cout << "Address: " << i << "\t\t" << "Value: " << *i << "\n";
+    }
 }
