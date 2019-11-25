6d657cf942df49f5edc3df30b4c48e66e432c92f
Viktor Rosvall <virosen@yahoo.se>
2019-11-25 16:33:50 +0100

    implemented random number function

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -34,42 +34,42 @@ struct buy_order
     }
 };
 
+int get_random_int();
+
+
 int main()
 {
-    std::random_device generator;
-    std::uniform_int_distribution<int> rnd(15, 30);
-
     p_queue<sell_order> sell_orders;
     p_queue<buy_order> buy_orders;
 
     // Generate sell orders
     for (int i = 0; i < 7; i++)
     {
-        sell_orders.push({"Erik Pendel", rnd(generator),});
+        sell_orders.push({"Erik Pendel", get_random_int()});
     }
 
     for (int i = 0; i < 7; i++)
     {
-        sell_orders.push({"Jarl Wallenburg", rnd(generator),});
+        sell_orders.push({"Jarl Wallenburg", get_random_int()});
     }
     for (int i = 0; i < 7; i++)
     {
-        sell_orders.push({"Joakim von Anka", rnd(generator),});
+        sell_orders.push({"Joakim von Anka", get_random_int()});
     }
 
     // Generate buy orders
     for (int i = 0; i < 7; i++)
     {
-        buy_orders.push({"Erik Pendel", rnd(generator),});
+        buy_orders.push({"Erik Pendel", get_random_int()});
     }
 
     for (int i = 0; i < 7; i++)
     {
-        buy_orders.push({"Jarl Wallenburg", rnd(generator),});
+        buy_orders.push({"Jarl Wallenburg", get_random_int()});
     }
     for (int i = 0; i < 7; i++)
     {
-        buy_orders.push({"Joakim von Anka", rnd(generator),});
+        buy_orders.push({"Joakim von Anka", get_random_int()});
     }
 
 
@@ -82,15 +82,23 @@ int main()
         while (!buy_orders.empty())
         {
             auto buy_ord = buy_orders.pop();
-            if (sell_ord.price < buy_ord.max_price && sell_ord.seller_name != buy_ord.buyer_name)
+            if (sell_ord.price <= buy_ord.max_price && sell_ord.seller_name != buy_ord.buyer_name)
             {
                 std::cout << buy_ord.buyer_name << " bought from "
                           << sell_ord.seller_name << " price: "
                           << sell_ord.price << "\n";
-
+                break;
             }
         }
     }
 
     return 0;
+}
+
+int get_random_int()
+{
+    std::random_device generator;
+    std::uniform_int_distribution<int> rnd(10, 40);
+
+    return rnd(generator);
 }
