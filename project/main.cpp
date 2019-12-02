8172f95d71fc35fd830b12f9b2b76601756484e4
Viktor Rosvall <virosen@yahoo.se>
2019-12-02 21:35:25 +0100

    Added simple multi-threading

--- project/main.cpp
+++ project/main.cpp
@@ -1,50 +1,71 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <thread>
+
+#if defined(__linux__)
+#include "X11/Xlib.h"
+#endif
+
+sf::RectangleShape rectangle(sf::Vector2f(50.0f, 50.0f));
+
+void renderingFunction(sf::RenderWindow* window)
+{
+
+    // activate the window's context
+    window->setActive(true);
+
+    // the rendering loop
+    while (window->isOpen())
+    {
+        window->clear();
+        window->draw(rectangle);
+
+        // Update the window
+        window->display();
+    }
+}
 
 int main()
 {
-    // Get preferred resolution and BPP
-    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    // Create a window with the same pixel depth as the desktop
+    // Required for multi-threaded linux Xlib compilation
+#if defined(__linux__)
+    XInitThreads();
+#endif
+    // Get list of preferred resolutions and BPP, ordered best first
+    auto videoModes = sf::VideoMode::getFullscreenModes();
+
+    // Create a window with resolution slightly lower than full
     sf::RenderWindow window(
-            desktop,
+            videoModes[9],
             "SFML window",
-            sf::Style::Fullscreen
+            sf::Style::Close | sf::Style::Titlebar
     );
     window.setVerticalSyncEnabled(true); // VSync
+    window.setPosition(sf::Vector2i(0, 0));
+    window.setActive(false);
 
+    // Start rendering thread
+    std::thread renderingThread(&renderingFunction, &window);
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("resources/cute_image.jpeg"))
-        return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile("resources/dejavu/DejaVuSerif.ttf"))
-        return EXIT_FAILURE;
-    sf::Text text("Hello SFML", font, 50);
-
-
-    // Start the game loop
+    // Event and logic loop
+    sf::Event event;
     while (window.isOpen())
     {
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            rectangle.move(0,1);
+        }
+
         // Check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
         while (window.pollEvent(event))
         {
             // Close window: exit
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed
+                || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            {
                 window.close();
+                renderingThread.join();
+            }
         }
-
-        window.clear();
-        window.draw(sprite);
-        window.draw(text);
-
-        // Update the window
-        window.display();
     }
     return EXIT_SUCCESS;
 }
